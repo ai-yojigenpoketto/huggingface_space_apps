8567166a5cfc31c2e2f9872ccb664cd740c04798
ai-yojigenpokett <rzhou213@gmail.com>
2024-11-07 17:03:13 -0500

    create app

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
+import os
+from dotenv import load_dotenv
+from transformers import pipeline
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!!"
+# Load environment variables. Assumes that project contains .env file with API keys
+load_dotenv()
+hf_api_key = os.environ['HF_TOKEN']
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-demo.launch()
+get_completion = pipeline("summarization", model = 'sshleifer/distilbart-cnn-12-6')
+
+def summarize(input):
+    
+    output = get_completion(input)
+    return output[0]['summary_text']
+
+    
+demo = gr.Interface(fn=summarize, 
+                inputs=[gr.Textbox(label="Text to summarize", lines=6)],
+                outputs=[gr.Textbox(label="Result", lines=3)],
+                title="Text summarization with distilbart-cnn",
+                description="Summarize any text using the `shleifer/distilbart-cnn-12-6` model under the hood!"
+                )
+demo.launch(share=True)
+
+
+
+if __name__ == "__main__":
+    main()
